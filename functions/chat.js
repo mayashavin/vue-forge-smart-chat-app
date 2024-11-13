61b047ad8176fd4e0f089b0e54c876ccadc9aca5
Maya Shavin <dpnminh@gmail.com>
2024-11-14 01:34:34 +0200

    add: start chat

--- functions/chat.js
+++ functions/chat.js
@@ -1,10 +1,44 @@
-export const handler = async () => {
+import {
+  ChatOpenAI
+} from "@langchain/openai";
+import {
+  ChatPromptTemplate
+} from "@langchain/core/prompts";
 
+export const handler = async () => {
+  const llm = new ChatOpenAI({
+    model: "gpt-4o",
+    temperature: 0,
+    // other params...
+  });
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "You are a helpful assistant that translates {input_language} to {output_language}.",
+    ],
+    ["human", "{input}"],
+  ]);
+
+  const chain = prompt.pipe(llm);
+  const aiMsg = await chain.invoke({
+    input_language: "English",
+    output_language: "German",
+    input: "I love programming.",
+  });
+
+  // const aiMsg = await llm.invoke([{
+  //     role: "system",
+  //     content: "You are a helpful assistant that translates English to French. Translate the user sentence.",
+  //   },
+  //   {
+  //     role: "user",
+  //     content: "I love programming.",
+  //   },
+  // ]);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Chat function'
-    }),
+    body: JSON.stringify(aiMsg.content)
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
-<script setup lang="js"></script>
+<script setup lang="js">
+import { ref } from 'vue'
 
-<template></template>
+const data = ref('')
+
+const startChat = async () => {
+  console.log('start chat')
+  const result = await fetch('.netlify/functions/chat').then((res) => res.json())
+  console.log(result)
+  data.value = result
+}
+</script>
+
+<template>
+  <div>
+    <button @click="startChat">Start Chat</button>
+    <p>{{ data }}</p>
+  </div>
+</template>
